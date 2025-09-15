490aa3e50d88d2cd6e6369d0277080f450c8b0c6
akshataik05 <akshatakallimath05@gmail.com>
2025-09-15 22:26:27 +0530

    Time: 18 ms (95.69%), Space: 48.1 MB (65.07%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.java
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -1,39 +1,45 @@
 class Solution {
-  public String[] spellchecker(String[] wordlist, String[] queries) {
-    List<String> ans = new ArrayList<>();
-    Map<String, String> dict = new HashMap<>();
-
-    for (final String word : wordlist) {
-      dict.putIfAbsent(word, word);
-      dict.putIfAbsent(lowerKey(word), word);
-      dict.putIfAbsent(vowelKey(word), word);
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> s = new HashSet<>();
+        Map<String, String> low = new HashMap<>();
+        Map<String, String> pat = new HashMap<>();
+        for (String w : wordlist) {
+            s.add(w);
+            String t = w.toLowerCase();
+            low.putIfAbsent(t, w);
+            pat.putIfAbsent(f(t), w);
+        }
+        int m = queries.length;
+        String[] ans = new String[m];
+        for (int i = 0; i < m; ++i) {
+            String q = queries[i];
+            if (s.contains(q)) {
+                ans[i] = q;
+                continue;
+            }
+            q = q.toLowerCase();
+            if (low.containsKey(q)) {
+                ans[i] = low.get(q);
+                continue;
+            }
+            q = f(q);
+            if (pat.containsKey(q)) {
+                ans[i] = pat.get(q);
+                continue;
+            }
+            ans[i] = "";
+        }
+        return ans;
     }
 
-    for (final String query : queries)
-      if (dict.containsKey(query))
-        ans.add(dict.get(query));
-      else if (dict.containsKey(lowerKey(query)))
-        ans.add(dict.get(lowerKey(query)));
-      else if (dict.containsKey(vowelKey(query)))
-        ans.add(dict.get(vowelKey(query)));
-      else
-        ans.add("");
-
-    return ans.toArray(new String[0]);
-  }
-
-  private String lowerKey(final String word) {
-    return "$" + word.toLowerCase();
-  }
-
-  private String vowelKey(final String word) {
-    StringBuilder sb = new StringBuilder();
-    for (final char c : word.toCharArray())
-      sb.append(isVowel(c) ? '*' : Character.toLowerCase(c));
-    return sb.toString();
-  }
-
-  private boolean isVowel(char c) {
-    return "aeiouAEIOU".indexOf(c) != -1;
-  }
+    private String f(String w) {
+        char[] cs = w.toCharArray();
+        for (int i = 0; i < cs.length; ++i) {
+            char c = cs[i];
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                cs[i] = '*';
+            }
+        }
+        return String.valueOf(cs);
+    }
 }
