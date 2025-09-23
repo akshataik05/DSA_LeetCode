b5e6e137efa615bc182f7d19079913b10d3842d5
akshataik05 <akshatakallimath05@gmail.com>
2025-09-24 00:06:50 +0530

    Time: 0 ms (100%), Space: 41.4 MB (83.19%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,41 @@
+class Solution 
+{
+    /**
+     * Compares two version strings.
+     * @param version1 The first version string (e.g., "1.2.3")
+     * @param version2 The second version string (e.g., "1.2.4")
+     * @return -1 if version1 < version2, 1 if version1 > version2, 0 if equal
+     */
+    public int compareVersion(String version1, String version2) 
+    {
+        int version1Length = version1.length();
+        int version2Length = version2.length();
+      
+        int index1 = 0;
+        int index2 = 0;
+    
+        while (index1 < version1Length || index2 < version2Length) 
+        {
+            
+            int revision1 = 0;
+            while (index1 < version1Length && version1.charAt(index1) != '.') 
+            {
+                revision1 = revision1 * 10 + (version1.charAt(index1) - '0');
+                index1++;
+            }
+            int revision2 = 0;
+            while (index2 < version2Length && version2.charAt(index2) != '.') 
+            {
+                revision2 = revision2 * 10 + (version2.charAt(index2) - '0');
+                index2++;
+            }
+            if (revision1 != revision2) 
+            {
+                return revision1 < revision2 ? -1 : 1;
+            }
+            index1++;
+            index2++;
+        }
+        return 0;
+    }
+}
