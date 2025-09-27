03166da7a2deb8ed817a9f8d195ee6596ad061ac
akshataik05 <akshatakallimath05@gmail.com>
2025-09-27 18:45:13 +0530

    Time: 13 ms (47.28%), Space: 54.7 MB (36.41%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
@@ -5,8 +5,7 @@ class Solution
     int ans = 0;
     Map<Integer, Integer> count = new HashMap<>();
 
-    for (int[] domino : dominoes) 
-    {
+    for (int[] domino : dominoes) {
       int key = Math.min(domino[0], domino[1]) * 10 + Math.max(domino[0], domino[1]);
       ans += count.getOrDefault(key, 0);
       count.merge(key, 1, Integer::sum);
