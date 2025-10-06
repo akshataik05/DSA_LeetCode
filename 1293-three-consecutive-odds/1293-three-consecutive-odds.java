05f831568fd7da3d318eb337b9b05b3722a7729c
akshataik05 <akshatakallimath05@gmail.com>
2025-10-06 18:44:56 +0530

    Time: 0 ms (100%), Space: 42 MB (95.48%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public boolean threeConsecutiveOdds(int[] arr)
+    {
+        int count=0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i]%2 != 0)
+            {
+                count=count+1;
+                if(count==3)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        return false;
+    }
+}
