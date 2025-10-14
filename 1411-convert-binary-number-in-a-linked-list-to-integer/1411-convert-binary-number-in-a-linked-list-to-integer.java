cb8ce430bf5b5c55cd2dc677e9dd80df0bd5be5e
akshataik05 <akshatakallimath05@gmail.com>
2025-10-14 21:40:52 +0530

    Time: 0 ms (100%), Space: 41.3 MB (38.23%) - LeetHub

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,12 @@
+class Solution 
+{
+  public int getDecimalValue(ListNode head) 
+  {
+    int ans = 0;
+
+    for (; head != null; head = head.next)
+      ans = ans * 2 + head.val;
+
+    return ans;
+  }
+}
