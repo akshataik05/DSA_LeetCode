0fdbde4d6234eab70e93694836d433eb0fe529ec
akshataik05 <akshatakallimath05@gmail.com>
2025-10-28 16:37:45 +0530

    Time: 1 ms (83.13%), Space: 42.1 MB (69.28%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+  public int countValidSelections(int[] nums) 
+  {
+    final int n = nums.length;
+    int ans = 0;
+    int[] prefix = new int[n];
+    int[] suffix = new int[n]; 
+
+    for (int i = 1; i < n; ++i)
+      prefix[i] = prefix[i - 1] + nums[i - 1];
+
+    for (int i = n - 2; i >= 0; --i)
+      suffix[i] = suffix[i + 1] + nums[i + 1];
+
+    for (int i = 0; i < n; ++i) 
+    {
+      if (nums[i] > 0)
+        continue;
+      if (prefix[i] == suffix[i])
+        ans += 2;
+      if (Math.abs(prefix[i] - suffix[i]) == 1)
+        ans += 1; 
+    }
+    return ans;
+  }
+}
