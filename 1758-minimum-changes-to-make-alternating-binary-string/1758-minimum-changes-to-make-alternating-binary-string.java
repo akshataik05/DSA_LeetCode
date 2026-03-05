d5eb2bfc815099e046e85093b517a47cc16234eb
Akshata I Kallimath <akshatakallimath05@gmail.com>
2026-03-05 22:07:41 +0530

    Time: 5 ms (53.53%), Space: 43.5 MB (95.87%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,12 +1,19 @@
-class Solution 
-{
-  public int minOperations(String s) 
-  {
-    int cost10 = 0;
-    for (int i = 0; i < s.length(); ++i)
-      if (s.charAt(i) - '0' == i % 2)
-        ++cost10;
-    final int cost01 = s.length() - cost10;
-    return Math.min(cost10, cost01);
-  }
+class Solution {
+    public int minOperations(String s) {
+        int change1 = 0;
+        int change2 = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(i % 2 == 0){
+                if(s.charAt(i) != '0') change1++;
+                if(s.charAt(i) != '1') change2++;
+            }
+            else{
+                if(s.charAt(i) != '1') change1++;
+                if(s.charAt(i) != '0') change2++;
+            }
+        }
+
+        return Math.min(change1, change2);
+    }
 }
